17a300e5e95eba4c97362b91f502b7c1c367b936
Udon-japanese <odaniyoshito@gmail.com>
2024-03-13 09:20:28 +0900

    cron test

--- src/app/api/pushMedicineReminder/route.ts
+++ src/app/api/pushMedicineReminder/route.ts
@@ -7,79 +7,99 @@ import { getDateInTimezone } from '@/utils/getDateInTimezone';
 
 export async function GET() {
   try {
-    const currentDate = getDateInTimezone(new Date());
-    console.log('currentDate', currentDate)
-    const currentHours = getHours(currentDate);
-    const currentMinutes = getMinutes(currentDate);
-    const currentTimeInMinutes = currentHours * 60 + currentMinutes;
-    const completedOrSkippedMedicineRecords = await prisma.medicineRecord.findMany({
-      where: {
-        OR: [{ isCompleted: true }, { isSkipped: true }],
-        scheduledIntakeTime: currentTimeInMinutes,
-      },
-      select: {
-        medicineId: true,
-      },
-    });
-    const completedOrSkippedMedicineIds = completedOrSkippedMedicineRecords.map(
-      (m) => m.medicineId,
-    );
-    const currentMedicines = await prisma.medicine.findMany({
-      where: {
-        NOT: {
-          id: {
-            in: completedOrSkippedMedicineIds,
-          },
-        },
-        notify: true,
-        intakeTimes: {
-          some: {
-            time: currentTimeInMinutes,
-          },
-        },
-      },
-      include: {
-        intakeTimes: true,
-        memo: true,
-      },
-    });
+    // const currentDate = getDateInTimezone(new Date());
+    // console.log('currentDate', currentDate)
+    // const currentHours = getHours(currentDate);
+    // const currentMinutes = getMinutes(currentDate);
+    // const currentTimeInMinutes = currentHours * 60 + currentMinutes;
+    // const completedOrSkippedMedicineRecords = await prisma.medicineRecord.findMany({
+    //   where: {
+    //     OR: [{ isCompleted: true }, { isSkipped: true }],
+    //     scheduledIntakeTime: currentTimeInMinutes,
+    //   },
+    //   select: {
+    //     medicineId: true,
+    //   },
+    // });
+    // const completedOrSkippedMedicineIds = completedOrSkippedMedicineRecords.map(
+    //   (m) => m.medicineId,
+    // );
+    // const currentMedicines = await prisma.medicine.findMany({
+    //   where: {
+    //     NOT: {
+    //       id: {
+    //         in: completedOrSkippedMedicineIds,
+    //       },
+    //     },
+    //     notify: true,
+    //     intakeTimes: {
+    //       some: {
+    //         time: currentTimeInMinutes,
+    //       },
+    //     },
+    //   },
+    //   include: {
+    //     intakeTimes: true,
+    //     memo: true,
+    //   },
+    // });
 
-    const userMedicines = new Map<
-      string,
-      {
-        name: string;
-        imageUrl?: string;
-      }[]
-    >();
-    for (const medicine of currentMedicines) {
-      const userId = medicine.userId;
-      const medicineName = medicine.name;
-      const imageId = medicine?.memo?.imageId;
-      let imageUrl: string | undefined;
+    // const userMedicines = new Map<
+    //   string,
+    //   {
+    //     name: string;
+    //     imageUrl?: string;
+    //   }[]
+    // >();
+    // for (const medicine of currentMedicines) {
+    //   const userId = medicine.userId;
+    //   const medicineName = medicine.name;
+    //   const imageId = medicine?.memo?.imageId;
+    //   let imageUrl: string | undefined;
 
-      if (!userMedicines.has(userId)) {
-        userMedicines.set(userId, []);
-      }
+    //   if (!userMedicines.has(userId)) {
+    //     userMedicines.set(userId, []);
+    //   }
 
-      const userMedicineList = userMedicines.get(userId);
-      const userHasMedicineWithImage = userMedicineList?.some((m) => m.imageUrl);
+    //   const userMedicineList = userMedicines.get(userId);
+    //   const userHasMedicineWithImage = userMedicineList?.some((m) => m.imageUrl);
 
-      if (imageId && !userHasMedicineWithImage) {
-        const imgUrl = await getImageUrlByIdServer(imageId);
-        if (imgUrl) imageUrl = imgUrl;
-      }
+    //   if (imageId && !userHasMedicineWithImage) {
+    //     const imgUrl = await getImageUrlByIdServer(imageId);
+    //     if (imgUrl) imageUrl = imgUrl;
+    //   }
 
-      userMedicines.get(userId)?.push({ imageUrl, name: medicineName });
-    }
+    //   userMedicines.get(userId)?.push({ imageUrl, name: medicineName });
+    // }
 
-    for (const [userId, medicines] of userMedicines) {
-      const pushSubscriptions = await prisma.pushSubscription.findMany({
-        where: {
-          userId,
-        },
-      });
-      const formattedCurrentDate = format(currentDate, 'M月dd日 H:mm');
-      if (!pushSubscriptions || pushSubscriptions.length === 0) continue;
+    // for (const [userId, medicines] of userMedicines) {
+    //   const pushSubscriptions = await prisma.pushSubscription.findMany({
+    //     where: {
+    //       userId,
+    //     },
+    //   });
+    //   const formattedCurrentDate = format(currentDate, 'M月dd日 H:mm');
+    //   if (!pushSubscriptions || pushSubscriptions.length === 0) continue;
+
+    //   for (const subscription of pushSubscriptions) {
+    //     const pushSubscription = {
+    //       endpoint: subscription.endpoint,
+    //       keys: {
+    //         auth: subscription.authKey,
+    //         p256dh: subscription.p256dhkey,
+    //       },
+    //     };
+
+    //     const payload = JSON.stringify({
+    //       body: `${medicines.map((m) => m.name).join(' ')} ${formattedCurrentDate}`,
+    //       image: medicines.find((m) => m.imageUrl)?.imageUrl,
+    //     });
+
+    //     webPush.sendNotification(pushSubscription, payload);
+    //   }
+    // }
+
+      const pushSubscriptions = await prisma.pushSubscription.findMany();
 
       for (const subscription of pushSubscriptions) {
         const pushSubscription = {
@@ -91,15 +111,14 @@ export async function GET() {
         };
 
         const payload = JSON.stringify({
-          body: `${medicines.map((m) => m.name).join(' ')} ${formattedCurrentDate}`,
-          image: medicines.find((m) => m.imageUrl)?.imageUrl,
+          body: `てすと`,
         });
 
         webPush.sendNotification(pushSubscription, payload);
-      }
     }
 
-    return new NextResponse('throw Error', { status: 500 });
+
+    return new NextResponse('OK', { status: 200 });
   } catch (err) {
     console.error(err);
     return new NextResponse('Internal Server Error', { status: 500 });
