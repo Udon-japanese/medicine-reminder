045144a1ab1265be2435e2633504983490a55eaf
Udon-japanese <odaniyoshito@gmail.com>
2024-03-13 11:52:48 +0900

    cron test

--- src/app/api/pushMedicineReminder/route.ts
+++ src/app/api/pushMedicineReminder/route.ts
@@ -7,7 +7,14 @@ import { getDateInTimezone } from '@/utils/getDateInTimezone';
 
 export const maxDuration = 60;
 export const dynamic = 'force-dynamic'
-export async function GET() {
+export async function GET(req: Request) {
+  const authHeader = req.headers.get('authorization');
+  if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+    return new Response('Unauthorized', {
+      status: 401,
+    });
+  }
+
   try {
     // const currentDate = getDateInTimezone(new Date());
     // console.log('currentDate', currentDate)
